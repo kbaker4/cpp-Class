1c87277c021215027a16cc14a53b971b65122795
unknown <kevin.r.baker@KBAKERDT2.fmi.lcl>
2015-07-17 13:00:32 -0400

    Some work to Card.h and Card.cpp for member variables, along with get and set for them all.

--- Rolodex/Rolodex/Card.cpp
+++ Rolodex/Rolodex/Card.cpp
@@ -9,3 +9,19 @@ Card::Card()
 Card::~Card()
 {
 }
+
+void Card::setFirstname(string fname){
+  firstname = fname;
+}
+void Card::setLastname(string lname){
+  lastname = lname;
+}
+void Card::setOccupation(string occ){
+  occupation = occ;
+}
+void Card::setAddress(string addr){
+  address = addr;
+}
+void Card::setPhoneNumber(string phone){
+  phoneNumber = phone;
+}
--- Rolodex/Rolodex/Card.h
+++ Rolodex/Rolodex/Card.h
@@ -1,9 +1,25 @@
 #pragma once
+using namespace std;
+#include <string>
 class Card
 {
 public:
   Card();
   ~Card();
+
+  string firstname, lastname, occupation, address, phoneNumber;
+
+  string getFirstname() { return firstname; }
+  string getLastname() { return lastname; }
+  string getOccupation() { return occupation; }
+  string getAddress() { return address; }
+  string getPhoneNumber() { return phoneNumber; }
+
+  void setFirstname(string fname);
+  void setLastname(string lname);
+  void setOccupation(string occ);
+  void setAddress(string addr);
+  void setPhoneNumber(string phone);
 
   //std::string for first name, last name, occupation, address(one string), and phone number
   //get/set member functions to allow get/set of the member variables ^^^
